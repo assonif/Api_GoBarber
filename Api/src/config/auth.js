b531c731dbb20efaa39619953e3be52cb01caf9f
Fabio Furtado <fabioassoni06@gmail.com>
2020-04-23 23:53:56 -0300

    Typescript implantado

--- Api/src/config/auth.js
+++
@@ -1,4 +0,0 @@
-export default {
-  secret: '836c5dc73405502263f7ba52afd42348',
-  expiresIn: '7d',
-};
--- Api/src/config/sentry.js
+++
@@ -1,3 +0,0 @@
-export default {
-  dsn: 'https://54b4c5d2a2c84b8e868b055f6a86f39e@sentry.io/1813794',
-};
